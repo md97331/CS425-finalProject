3eca51dec512b3bcd0a7ff08c3c45dde50e62427
Mario V. Diaz <marioalan84@gmail.com>
2023-04-22 01:12:25 -0500

    new files previous commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import classesSQL.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Passenger testPassenger = new Passenger(1023,"Carlos","Dominguez","PASSWORD","Masculine","10-08-2003","192382726",21,"9021","9015521726");
+        testPassenger.printData();
+
+    }
+}
