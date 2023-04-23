b072714bb73cda578635be4220279d560a6a1317
Jio Velazquez <jvelazquez5@hawk.iit.edu>
2023-04-22 20:35:20 -0500

    Added Functions

--- funcCpp/passenger.cpp
+++
@@ -1,6 +0,0 @@
-#include "passenger.h"
-
-void passenger::printData() {
-    std::cout << "\nPassenger Name: " << firstName << " " << lastName << "\nGender: " << gender << "\tAge: " << age << "\n";
-    std::cout << "Cellphone: "<< cellphone << "\tPassport: " << passport;
-}
