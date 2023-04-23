fdefcb447edd7646b2818d1657d17d56884239d1
Jio Velazquez <jvelazquez5@hawk.iit.edu>
2023-04-23 11:32:05 -0500

    ed

--- main.java
+++ main.java
@@ -1,7 +1,13 @@
-public class main {
-    static void Main(){
-        new passenger = passenger(name, lastname, id);
+import classes_SQL.*;
+import func_Helpers.*;
 
-        passenger.printData();
+
+public class Main {
+    public static void main(String[] args) {
+        Passenger testPassenger = new Passenger(1023,"Carlos","Dominguez","PASSWORD","Masculine","10-08-2003","192382726",21,"9021","9015521726");
+        testPassenger.printData();
+
+
+
     }
 }
