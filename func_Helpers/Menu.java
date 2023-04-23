85be850c6fdc901289fb0a5aa49835cdf88b7660
Jerry Lee <jerry53165@gmail.com>
2023-04-23 13:16:35 -0500

    add display menu

--- func_Helpers/Menu.java
+++ func_Helpers/Menu.java
@@ -3,6 +3,8 @@ package func_Helpers;
 import java.sql.*;
 import java.util.Scanner;
 import classes_SQL.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.System.exit;
 
@@ -77,5 +79,62 @@ public class Menu {
         }
         User returnedUser = new User(username, password, -1);
     return returnedUser;
+    }
+
+    public Map<Integer, String> initAdminMenu() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Passenger");
+        map.put(2, "Flight");
+        map.put(3, "Connection");
+        map.put(4, "FlightClass");
+        map.put(5, "Ticket");
+        map.put(6, "Payment");
+        map.put(7, "Quit");
+        return map;
+    }
+
+    public Map<Integer, String> initAdminSubMenu() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "View");
+        map.put(2, "Edit");
+        map.put(3, "Add");
+        map.put(4, "Delete");
+        map.put(5, "Quit");
+        return map;
+    }
+
+    public Map<Integer, String> initSql() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Select * from ");//CONDITION ONLY
+        map.put(2, "Update set");//CONDITION & VALUE
+        map.put(3, "Inset into");//VALUE ONLY
+        map.put(4, "Delete from");//CONDITION ONLY
+        return map;
+    }
+
+    public Map<Integer, String> initUserMenu() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Search Flight");
+        map.put(2, "Search Passenger");
+        map.put(3, "Book Ticket");
+        map.put(4, "Quit");
+        return map;
+    }
+
+    public static void displayMenu(User userAccount) {
+        System.out.println("\n----------- Menu----------");
+        if(userAccount.getAdmin() == 1){// admin
+            Map<Integer, String> adminMenuTitle = new Menu().initAdminMenu();
+            for(int i = 1; i<= adminMenuTitle.size(); i++)
+                System.out.println("(" + i + ") " + adminMenuTitle.get(i));
+        }else if (userAccount.getAdmin() == 2) { //user
+            Map<Integer, String> userMenuTitle = new Menu().initUserMenu();
+            for(int i = 1; i<= userMenuTitle.size(); i++)
+                System.out.println("(" + i + ") " + userMenuTitle.get(i));
+        }else{
+            System.out.println("illegal status");
+            exit(0);
+        }
+
     }
 }
